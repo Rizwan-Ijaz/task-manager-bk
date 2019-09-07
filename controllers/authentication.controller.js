1daf1db09a99cdb8b56cac17bc91fa5a70387a26
Rizwan-Ijaz <rizwanijaz2@gmail.com>
2019-09-07 23:06:31 +0500

    Signin method updated

--- controllers/authentication.controller.js
+++ controllers/authentication.controller.js
@@ -3,30 +3,28 @@ const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const jwtBlacklist = require('jwt-blacklist')(jwt);
 
-exports.signIn = (req, res) => {
+exports.signIn = async (req, res) => {
     const requestBody = req.body;
 
     try {
-        User.findOne({ email: requestBody.email, isActive: true, isDeleted: false })
-            .exec((error, user) => {
-                if (error) {
-                    res.status(400).json({ message: error.message });
-                    return;
-                } else {
-                    if (user) {
-                        bcrypt.compare(requestBody.password, user.password, (error, success) => {
-                            if (success) {
-                                const token = jwt.sign({ user: user._id }, 'TM', { expiresIn: process.env.EXPIRY });
-                                res.status(200).json({ user, token });
-                            } else {
-                                res.status(200).json({ message: 'Password did not match' });
-                            }
-                        });
+
+        const user = await User.findOne({ email: requestBody.email, isActive: true, isDeleted: false });
+
+        if (user) {
+            if (user) {
+                bcrypt.compare(requestBody.password, user.password, (error, success) => {
+                    if (success) {
+                        const token = jwt.sign({ user: user._id }, process.env.SECURE_PASSWORD, { expiresIn: process.env.EXPIRY });
+                        res.status(200).json({ user, token });
                     } else {
-                        res.status(200).json({ message: 'Email did not match' });
+                        res.status(200).json({ message: 'Password did not match' });
                     }
-                }
-            });
+                });
+            } else {
+                res.status(200).json({ message: 'Email did not match' });
+            }
+        }
+
     } catch (error) {
         res.status(500).json({ message: error })
     }
