3199a15db46f55c541654133b24fde844d575c96
rizwan <rizwan.ijaz@dmechs.com>
2019-12-30 18:26:18 +0500

    signIn function updated

--- controllers/authentication.controller.js
+++ controllers/authentication.controller.js
@@ -2,31 +2,30 @@ const User = require('../models/users.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const jwtBlacklist = require('jwt-blacklist')(jwt);
+require('dotenv').config();
 
 exports.signIn = async (req, res) => {
     const requestBody = req.body;
 
     try {
 
-        const user = await User.findOne({ email: requestBody.email, isActive: true, isDeleted: false });
+        const user = await User.findOne({email: requestBody.email, isActive: true, isDeleted: false});
 
         if (user) {
-            if (user) {
-                bcrypt.compare(requestBody.password, user.password, (error, success) => {
-                    if (success) {
-                        const token = jwt.sign({ user: user._id }, process.env.SECURE_PASSWORD, { expiresIn: process.env.EXPIRY });
-                        res.status(200).json({ user, token });
-                    } else {
-                        res.status(200).json({ message: 'Password did not match' });
-                    }
-                });
-            } else {
-                res.status(200).json({ message: 'Email did not match' });
-            }
+            bcrypt.compare(requestBody.password, user.password, (error, success) => {
+                if (success) {
+                    const token = jwt.sign({user: user._id}, process.env.SECURE_PASSWORD, {expiresIn: process.env.EXPIRY});
+                    res.status(200).json({user, token});
+                } else {
+                    res.status(401).json({message: 'Invalid Credentials'});
+                }
+            });
+        } else {
+            res.status(401).json({message: 'Invalid Credentials'});
         }
 
     } catch (error) {
-        res.status(500).json({ message: error })
+        res.status(500).json({message: error})
     }
 }
 
@@ -34,8 +33,8 @@ exports.signOut = (req, res) => {
     const token = req.headers['access-token'];
     try {
         jwtBlacklist.blacklist(token);
-        res.status(200).json({ message: 'User is logged-out' });
+        res.status(200).json({message: 'User is logged-out'});
     } catch (error) {
-        res.status(500).json({ message: error })
+        res.status(500).json({message: error})
     }
 }
